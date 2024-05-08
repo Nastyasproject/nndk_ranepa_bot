252aa30b04703cee93d9e3b017ae014e52375348
ddavydova777 <152385372+ddavydova777@users.noreply.github.com>
2024-05-08 10:38:02 +0000

    Создали пакет handlers и описали маршрутизацию

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+#from aiogram.filters.command import Command
 from config import TOKEN
+from handlers import register_message_handler
 logging.basicConfig(level=logging.DEBUG)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command('start'))
-async def process_start_message(message: types.Message):
-  """Приветствие"""
-  await message.answer('Привет!')
-  logging.debug('Привет!')
+# @dp.message(Command('start'))
+# async def process_start_message(message: types.Message):
+#   """Приветствие"""
+#   await message.answer('Привет!')
+#   logging.debug('Привет!')
 
 
-@dp.message()
-async def echo(message: types.Message):
-  """Эхо-ответ"""
-  await message.answer(message.text)
+# @dp.message()
+# async def echo(message: types.Message):
+#   """Эхо-ответ"""
+#   await message.answer(message.text)
   
 async def start():
   await dp.start_polling(bot)
